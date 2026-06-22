09271cd6b094ccb07eda91793b3b0d2dd1bc8155
NejcBohinjc <nbohinjc@gmail.com>
2026-06-22 23:20:14 +0200

    Update wrong answer data

--- quiz-app/wrong-data.js
+++ quiz-app/wrong-data.js
@@ -2,6 +2,5 @@
    Questions answered incorrectly at some point and not yet corrected.
    Commit this file to GitHub to sync across machines. */
 window.WRONG_DATA = {
-  "111": true,
   "112": true
 };
